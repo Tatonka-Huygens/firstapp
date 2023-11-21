bc20c7ddd7ab0eabfa47da1ab432fa8357c985f0
Luis Manyari <lmanyari@hotmail.com>
2023-11-21 05:31:35 -0500

    :P~ third commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import Heading from "./Heading";
 function App() {
   return (
     <div className="App">
-       <Heading firstName="Bob"/>
+      <Heading firstName="Any name other than Bob" />
+      <Heading firstName="Jack" />
     </div>
   );
 }
 
 export default App;
-
-
